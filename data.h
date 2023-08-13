ea1f3ca62b85da0ced3cc37cdf84b7ffccce0599
lvntky <klevent1903@gmail.com>
2023-08-13 22:46:11 +0300

    refactor

--- data.h
+++
@@ -1,8 +0,0 @@
-#ifndef extern_
-#define extern_ extern
-#endif
-#include <stdio.h>
-
-extern_ int line;
-extern_ int putback_token;
-extern_ FILE *infile;
--- decl.h
+++
@@ -1,5 +0,0 @@
-
-#ifndef DECL_H
-#define DECL_H
-int scan(struct token *t);
-#endif
